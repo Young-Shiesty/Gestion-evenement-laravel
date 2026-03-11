d830fb3efa55282a722a3b9ef8435ccba048349b
Youngshiesty <mamzf18@gmail.com>
2026-03-11 22:57:06 +0100

    Premier

---
+++ database/migrations/2026_03_11_213608_create_evenements_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('evenements', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('nom');
+            $table->text('description');
+            $table->string('lieu');
+            $table->string('categorie');
+            $table->date('date');
+            $table->time('heure');
+            $table->integer('nombre_max_participants');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('evenements');
+    }
+};
